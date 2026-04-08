3b89b65b9918583ea81cb44311b8c29c3c23a383
Ubuntu <ubuntu@ip-172-31-29-124.eu-north-1.compute.internal>
2026-04-08 18:05:58 +0000

    Add Tea to task list

--- init.sql
+++ init.sql
@@ -1,11 +1,6 @@
-CREATE TABLE IF NOT EXISTS tasks (
-  id INT PRIMARY KEY,
-  name TEXT,
-  status TEXT
-);
-
 INSERT INTO tasks VALUES
 (1, 'Study', 'done'),
 (2, 'Workout', 'pending'),
-(3, 'Meeting', 'done')
+(3, 'Meeting', 'done'),
+(4, 'Tea', 'pending')
 ON CONFLICT (id) DO NOTHING;
